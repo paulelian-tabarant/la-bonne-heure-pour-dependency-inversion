4e1bd1a5b640fb85c451698014de2502899d99a9
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-06-22 17:47:34 +0200

    feat: gérer la timezone pour considérer l'heure de Paris

--- src/main/kotlin/LaBonneHeurePour.kt
+++ src/main/kotlin/LaBonneHeurePour.kt
@@ -1,14 +1,20 @@
-import java.time.LocalTime
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class LaBonneHeurePour {
     fun demander(): String {
-        val heure = LocalTime.now().hour
+        val instantUtc = LocalDateTime.now()
+        val timezoneParis = ZoneId.of("Europe/Paris")
+        val zonedTimeParis = ZonedDateTime.of(instantUtc, timezoneParis)
+
+        val heure = zonedTimeParis.hour
 
         return when (heure) {
             in 5..9 -> "les croissants"
             in 12..13 -> "un Welsch"
-            in 17..19 -> "l'apéro"
-            in 19..21 -> "le dîner"
+            in 18..20 -> "l'apéro"
+            in 20..22 -> "le dîner"
             else -> "le café"
         }
     }
